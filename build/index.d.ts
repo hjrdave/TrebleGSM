ffb99c0fe84524e7eb619a8cc2a91c33134b1f82
David Sanders <dsanders_m633@outlook.com>
2023-02-08 19:28:38 -0500

    experimenting with namespaces

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,8 @@
 import TrebleGSM from "./TrebleGSM";
 export default TrebleGSM;
+export declare namespace TrebleGSM {
+    interface Options {
+        foo: string;
+        fee: number;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import TrebleGSM from "./TrebleGSM";
 
+
 export default TrebleGSM;
+
+export namespace TrebleGSM {
+    export interface Options {
+        foo: string;
+        fee: number;
+    }
+}
